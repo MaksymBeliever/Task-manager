e6f7bf21f41e765b01ed1654eb1c67fa364461e1
max <max.beliba@alternatestates.com>
2024-02-06 00:41:56 +0200

    Basic components were completed

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,5 +1,16 @@
 import "./Task.css";
+import classNames from "classnames";
+
+const STATUS = 'PLANNED';
 
 export default function Task({ title }) {
-    return <div className="task">{<title></title>}</div>;
+    return (
+        <div className="task">
+            <div>{title}</div>
+            <div className="bottomWrapper">
+                <div></div>
+                <div className={classNames("status", STATUS)}>{STATUS}</div>
+            </div>
+        </div>
+    );
 }
