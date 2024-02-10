23feec96952c7bd431d3697b3542c593c521e30a
max <max.beliba@alternatestates.com>
2024-02-11 00:08:57 +0200

    Was implemented ability to add new tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,18 @@
 import "./Task.css";
 import classNames from "classnames";
-
-const STATUS = 'PLANNED';
+import { useStore } from "../store.js";
 
 export default function Task({ title }) {
+    const task = useStore((store) =>
+        store.tasks.find((task) => task.title === title)
+    );
+
     return (
         <div className="task">
-            <div>{title}</div>
+            <div>{task.title}</div>
             <div className="bottomWrapper">
                 <div></div>
-                <div className={classNames("status", STATUS)}>{STATUS}</div>
+                <div className={classNames("status", task.state)}>{task.state}</div>
             </div>
         </div>
     );
