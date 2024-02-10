9795203d7a68d7c63a97292f6ec32fa83b4939aa
max <max.beliba@alternatestates.com>
2024-02-11 00:28:34 +0200

    Was implemented ability to remove tasks

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,29 @@
 import "./Task.css";
 import classNames from "classnames";
 import { useStore } from "../store.js";
+import { TrashOutline } from "react-ionicons";
 
 export default function Task({ title }) {
     const task = useStore((store) =>
         store.tasks.find((task) => task.title === title)
     );
 
+    const deleteTask = useStore((store) => store.deleteTask);
+
     return (
         <div className="task">
             <div>{task.title}</div>
             <div className="bottomWrapper">
-                <div></div>
+                <div>
+                    <button onClick={() => deleteTask(task.title)}>
+                        <TrashOutline
+                            color="#00000"
+                            title="delete"
+                            height="20px"
+                            width="20px"
+                        />
+                    </button>
+                </div>
                 <div className={classNames("status", task.state)}>{task.state}</div>
             </div>
         </div>
